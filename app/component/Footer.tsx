6be4b0e5f802565dea8eea9e6837f959de80ddd5
RidznaAP <107558138+RidznaAP@users.noreply.github.com>
2024-06-01 23:08:25 +0700

    feat:footer

---
+++ app/component/Footer.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Footer() {
+  return <div className="text-center py-10 text-black1 font-semibold text-xl">&copy; 2024 RidznaAP Portfolio. All Rights Reserved.</div>;
+}
