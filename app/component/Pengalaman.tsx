0720d0f8361a900da1023278e40929a31fa0ae7b
RidznaAP <107558138+RidznaAP@users.noreply.github.com>
2024-06-03 14:13:58 +0700

    refactor:about

---
+++ app/component/Pengalaman.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Pengalaman() {
+  return (
+    <div className="pt-[80px] md:pt-[200px] px-5 md:px-[100px] lg:px-[150px] flex flex-col items-center">
+      <h1 className="font-extrabold text-black1 text-[32px] text-center">
+        Pengalaman
+      </h1>
+      </div>
+  )
+}
